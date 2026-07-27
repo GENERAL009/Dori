cf43af74d552043c67e361dc154a975be46bddbf
GENERAL009 <abdulloh20062066@gmail.com>
2026-07-27 09:58:36 +0500

    telegram bot fix3

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 from logging.config import fileConfig
 from sqlalchemy import pool
@@ -6,6 +7,11 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 config = context.config
+
+database_url = os.getenv("DATABASE_URL")
+if database_url:
+    config.set_main_option("sqlalchemy.url", database_url)
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
